df05bd03b95a5760b761491a81dcb88bd3bfc105
valente1028 <97548063+valente1028@users.noreply.github.com>
2024-04-30 21:42:28 -0400

    Update CommentSection.js

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -5,33 +5,6 @@ const CommentSection = ({ comments, setComments, userId, postId }) => {
   console.log("comments: ", comments);
   const [newComment, setNewComment] = useState("");
 
-  const parseDate = (pdate) => {
-    let systemDate = new Date(Date.parse(pdate));
-    let userDate = new Date();
-    let diff = Math.floor((userDate - systemDate) / 1000);
-    if (diff < 60) {
-      return "less than a minute ago";
-    }
-    if (diff <= 90) {
-      return "one minute ago";
-    }
-    if (diff <= 3540) {
-      return Math.round(diff / 60) + " minutes ago";
-    }
-    if (diff <= 5400) {
-      return "1 hour ago";
-    }
-    if (diff <= 86400) {
-      return Math.round(diff / 3600) + " hours ago";
-    }
-    if (diff <= 129600) {
-      return "1 day ago";
-    }
-    if (diff < 604800) {
-      return Math.round(diff / 86400) + " days ago";
-    }
-    return "on " + systemDate;
-  };
 
   const handleAddComment = async (e) => {
     e.preventDefault();
@@ -62,7 +35,7 @@ const CommentSection = ({ comments, setComments, userId, postId }) => {
       {comments.length > 0 &&
         comments.map((comment) => (
           <p className="comment" key={comment.text}>
-            {comment.author == userId ? "You" : "@" + comment.author}: {comment.text} - {parseDate(comment.createdAt)}
+            {comment.author == userId ? "You" : "@" + comment.author}: {comment.text}
           </p>
         ))}
       <form onSubmit={handleAddComment}>
